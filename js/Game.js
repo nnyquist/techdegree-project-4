50ab3f77c77704d5fde8838070430f7662882785
NateTheOk <nnyquist@thinkbrg.com>
2020-03-16 15:16:11 -0400

    initialized classes Phrase and Game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  /**
+   * Constructor receives no parameters
+   * missed - tracks the number of missed guesses by player
+   * phrases - array of phrases for the game
+   * activePhrase - phrase object currently in play
+   */
+  constructor(){
+    this.missed = 0;
+    this.phrases = [];
+    this.activePhrase = null;
+  }
+}
